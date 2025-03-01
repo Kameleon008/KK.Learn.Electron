fc514308d4b6f2690d8606a42aebbfebb80da7af
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2025-03-01 20:44:51 +0100

    Remove OS X specific things from index.js

--- KK.Learn.Electron.ScreenRecorder/src/index.js
+++ KK.Learn.Electron.ScreenRecorder/src/index.js
@@ -28,23 +28,10 @@ const createWindow = () => {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Quit when all windows are closed
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
 });
 
 // In this file you can include the rest of your app's specific main process
